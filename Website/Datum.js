cf7de0ca9415d7583afc87a0cc366df1f598065a
Sem Kirkels <Sem.kirkels@student.pxl.be>
2022-05-20 19:16:18 +0200

    Branch fix
    Branch fix

---
+++ Website/Datum.js
@@ -0,0 +1,49 @@
+function displayTime()
+{
+    var dateTime = new Date();
+    var uren = addLeadingZeros(dateTime.getHours(), 2);
+    var min = addLeadingZeros(dateTime.getMinutes(), 2);
+    var sec = addLeadingZeros(dateTime.getSeconds(), 2);
+    var tempDag = dateTime.getDay();
+    let dag;
+
+    switch(tempDag)
+    {
+        case 0:
+            dag = "Zondag";
+            break;
+        case 1:
+            dag = "Maandag";
+            break;
+        case 2:
+            dag = "Dinsdag";
+            break;
+        case 3:
+            dag = "Woensdag";
+            break;
+        case 4:
+            dag = "Donderdag";
+            break;
+        case 5:
+            dag = "Vrijdag";
+            break;
+        case 6:
+            dag = "Zaterdag";
+            break;
+        default:
+            dag = "ErrorInScript";
+            break;
+    }
+    
+    document.getElementById('Uren').innerHTML = uren;
+    document.getElementById('Minuten').innerHTML = min;
+    document.getElementById('Seconden').innerHTML = sec;
+    document.getElementById('Dag').innerHTML = dag;
+}
+
+function addLeadingZeros(num, totalLength)
+{
+    return String(num).padStart(totalLength, '0');
+}
+
+setInterval(displayTime, 10);
